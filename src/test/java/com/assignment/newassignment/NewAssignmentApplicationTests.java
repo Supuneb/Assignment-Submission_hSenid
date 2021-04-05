ab5cbbb8c91fed3a02387d091143bafbc3198c7d
Supuneb <44536081+Supuneb@users.noreply.github.com>
2021-04-05 23:59:30 +0530

    With controllers

---
+++ src/test/java/com/assignment/newassignment/NewAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.newassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
